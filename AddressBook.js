bdf35ed5fd2d9efbc0d01ae0d320644de230d90f
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-04 00:40:40 +0530

    added validation for contact details

--- AddressBook.js
+++ AddressBook.js
@@ -11,53 +11,94 @@ class AddressBookMaster {
       this.email = params[7];
     }
   
-    //get and set for firstname
-    get firstName() { return this._firstName; }
-    set firstName(firstName) {
+    // get and set for firstname 
+  //first letter should be capital and min 3 letters
+  get firstName() { return this._firstName; }
+  set firstName(firstName) {
+    let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+    // test returns a boolen value
+    if (nameRegex.test(firstName))
       this._firstName = firstName;
-    }
+    else
+      throw "Invalid first Name";
+  }
   
-    //get and set for lastname
-    get lastName() { return this._lastName; }
-    set lastName(lastName) {
+   //get and set for lastname
+  //first letter should be capital and min 3 letters
+  get lastName() { return this._lastName; }
+  set lastName(lastName) {
+    let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+    // test returns a boolen value
+    if (nameRegex.test(lastName))
       this._lastName = lastName;
-    }
-  
+    else
+      throw "Invalid  last Name";
+  }
     //get and set for address
-    get address() { return this._address; }
-    set address(address) {
+  //minimum four characters
+  get address() { return this._address; }
+  set address(address) {
+    let addressRegex = RegExp("^[A-Za-z]{4,}$");
+    if (addressRegex.test(address))
       this._address = address;
-    }
+    else
+      throw "Invalid address ";
+  }
   
-    //get and set for city
-    get city() { return this._city; }
-    set city(city) {
+     //get and set for city
+  //minimum four characters
+  get city() { return this._city; }
+  set city(city) {
+    let cityRegex = RegExp("^[A-Za-z]{4,}$");
+    if (cityRegex.test(city))
       this._city = city;
-    }
-  
+    else
+      throw "Invalid city ";
+  }
+
     //get and set for state
-    get state() { return this._state; }
-    set state(state) {
+  //minimum four characters
+  get state() { return this._state; }
+  set state(state) {
+    let stateRegex = RegExp("^[A-Za-z0-9]{4,}$");
+    if (stateRegex.test(state))
       this._state = state;
-    }
+    else
+      throw "Invalid state";
+  }
   
-    //get and set for zip
-    get zip() { return this._zip; }
-    set zip(zip) {
+     //get and set for zip
+  //pin code of form 789 987
+  get zip() { return this._zip; }
+  set zip(zip) {
+    let zipRegex = RegExp("^[1-9]{3}[ ]*[0-9]{3}$");
+    if (zipRegex.test(zip))
       this._zip = zip;
-    }
+    else
+      throw "Invalid zip ";
+  }
   
-    //get and set for phoneNumber
-    get phoneNumber() { return this._phoneNumber; }
-    set phoneNumber(phoneNumber) {
+     //get and set for phoneNumber
+  //phone number should be of form 91 8149240833
+  get phoneNumber() { return this._phoneNumber; }
+  set phoneNumber(phoneNumber) {
+    let phoneRegex = RegExp("^[1-9]{2}[ ]{1}[0-9]{10}$");
+    if (phoneRegex.test(phoneNumber))
       this._phoneNumber = phoneNumber;
-    }
-  
+    else
+      throw "Invalid phone number";
+  }
+
     // get and set for email
-    get email() { return this._email; }
-    set email(email) {
+  //ac.xyz@gmail.com.in .xyz-optional  .in-optional
+  get email() { return this._email; }
+  set email(email) {
+    let emailRegex = RegExp("^([a-z0-9A-Z])+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-zA-Z]{2,}([.]{1}[a-z]{2,})?$");
+    if (emailRegex.test(email))
       this._email = email;
-    }
+    else
+      throw "Invalid email";
+  }
   
     //defining to string method
     toString() {
@@ -70,6 +111,11 @@ class AddressBookMaster {
   }
   
   //creating an instance and giving contact details
-  let contact_book = new AddressBookMaster("Prajakta", "Bramhe", "AshtyvinayakColony", "Nagpur", "Maharashtra", "440034",
-    "8149240833", "prajakta14bramhe@gmail.com");
+  try {
+  let contact_book = new AddressBookMaster("Prajakta", "Bramhe", "AshtyvinayakColony", "Nagpur", "Maharashtra", "445534",
+    "91 8149240833", "prajakta14bramhe@gmail.com");
   console.log(contact_book.toString());
+} catch (e) {
+    console.log(e);
+  }
+  
