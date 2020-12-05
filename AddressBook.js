c4f14bb70a2f17cf45cf70362b1bf59b1a41fb93
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-05 09:00:39 +0530

    serched person in a particular city or state using reduce, filter function

--- AddressBook.js
+++ AddressBook.js
@@ -177,13 +177,13 @@ function findTotalContacts(ContactsArray) {
 }
 ContactsArray.reduce(findTotalContacts, 1);
 console.log(
-  "--------------------Total Contacts Present in the AddressBookContacts array--------------------------   : " +
+  "--------------------Total Contacts Present in the ContactsArray array--------------------------   : " +
   ContactsPresenceTotal   
 );
 // check for duplicate Person details
 let CountDuplicate = 0;
 function CheckDuplicatesCount(contact_book) {
-  if (contact_book.firstName == "Nandha") CountDuplicate++;
+  if (contact_book.firstName == "Ekta") CountDuplicate++;
   return CountDuplicate;
 }
 //using foreach checking the count for each contact
@@ -196,6 +196,25 @@ else
   console.log(
     "------------------------------It is Duplicate Entry---------------------------------"
   );
+  //Ability to search person in particular City or State
+
+  //Search By City
+  console.log(
+    "\n------------ Search To get Contact by City Nagpur---------------------"
+  );
+  function ContactByCity(contact_book) {
+    if (contact_book.city == "Nagpur") console.log(contact_book.toString());
+  }
+  ContactsArray.filter(ContactByCity);
+
+  //Search By State
+  console.log(
+    "----------------Search Contact By State Maharashtra----------------"
+  );
+  let statecontact = ContactsArray.filter((contact_book) =>
+  contact_book.state.includes("Mharashtra")
+  );
+  console.log(statecontact.toString());
 } catch (e) {
   console.log(e);
 }
